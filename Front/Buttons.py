b072c0c5364c3b49f196196c80ce715c0bb5abb7
Ivan <ivanu1706@gmail.com>
2023-05-06 13:18:35 +1100

    Функционал Регистрации, не до конца

--- Front/Buttons.py
+++ Front/Buttons.py
@@ -2,7 +2,7 @@ from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
-inline_btn_cust = InlineKeyboardButton('Заказчик', callback_data='button_customer')
-inline_btn_cont = InlineKeyboardButton('Подрядчик', callback_data='button_contractor')
+inline_btn_cust = InlineKeyboardButton('Заказчик', callback_data='button_customer_sign')
+inline_btn_cont = InlineKeyboardButton('Подрядчик', callback_data='button_contractor_sign')
 inline_kb_roles = InlineKeyboardMarkup().add(inline_btn_cust).add(inline_btn_cont)
 
--- Front/Handlers.py
+++ Front/Handlers.py
@@ -1,12 +1,14 @@
 from aiogram import Dispatcher, types
 from Front.Buttons import  inline_kb_roles
+from backend.work_db import User
 
 async def start(msg: types.Message):
     await msg.answer('Привет, Я бот который поможет тебе заработать деньги на легких заданиях, а также продвинуть свои задания за оплату.\nВся инфа тут - /info')
 
 async def info(msg: types.Message):
-    await msg.answer('Много всякого')
-
+    await msg.answer('Этот бот помогает заработать деньги выполняя различные легкие задания или продвинуть свои задания, заплатив за их выполнение. '
+                     'Чтобы всё работало нужно зарегистрироваться (/sign_up) и выбрать роль, заказчик или подрядчик.'
+                     '')
 async def help(msg: types.Message):
     await msg.answer('Если у вас возникла проблема или вопрос пишите адмиимтраторам.')
 
@@ -14,7 +16,24 @@ async def sign_up(msg: types.Message):
     await msg.answer('Привет, Кто ты заказчик или Подрядчик?', reply_markup=inline_kb_roles)
 
 
+async def sign_customer(callback_query: types.CallbackQuery):
+    id = callback_query.from_user.id
+    user_name = callback_query.from_user.username
+    user = User(id)
+    user.sign_up(user_name, 'Z')
+    await callback_query.answer('Авторизация прошла успешно!')
+
+
+async def sign_contractor(callback_query: types.CallbackQuery):
+    id = callback_query.from_user.id
+    user_name = callback_query.from_user.username
+    user = User(id)
+    user.sign_up(user_name, 'P')
+    await callback_query.answer('Авторизация прошла успешно!')
+
 
 def register_handlers(dp: Dispatcher):
+    dp.register_callback_query_handler(sign_customer, lambda c: c.data == 'button_customer_sign')
+    dp.register_callback_query_handler(sign_contractor, lambda c: c.data == 'button_contractor_sign')
     dp.register_message_handler(sign_up, commands='sign_up')
     dp.register_message_handler(start, commands= 'start')
